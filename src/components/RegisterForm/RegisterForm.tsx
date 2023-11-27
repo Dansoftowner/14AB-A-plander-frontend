a5de8d47f1a352778883d8b59a5496c49c6503af
feherszabolcs <feherszabi2005@gmail.com>
2023-11-27 18:24:04 +0100

    :adhesive_bandage: Fix fullname regex

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, HStack, Heading, Input, InputGroup, InputLeftAddon, InputRightElement, Menu, MenuButton, MenuItem, MenuList, Stack, Text } from "@chakra-ui/react"
+import { Button, Divider, HStack, Heading, Input, InputGroup, InputLeftAddon, InputRightElement, Stack, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -20,7 +20,8 @@ export const RegisterForm = () => {
         username: z.string().min(5, { message: i18n.t('regForm-zodUsername') }).max(20),
         password: z.string().min(8, { message: t('regForm-zodPasswordLength') }).refine((str) => /[A-Z]/.test(str), { message: t('regForm-zodPassword') }).refine(str => /[0-9]/.test(str), { message: t('regForm-zodPassword') }),
         repeatedPassword: z.string(),
-        fullName: z.string().min(5, { message: t('regForm-zodFullname') }).max(40).refine(str => /^[a-zA-Zéáűúőóüö]+\s+[a-zA-zéáűúőóüö]+(\s[a-zA-zéáűúőóüö]+)*$/g.test(str), { message: t('regForm-zodInvalidName') }),
+        fullName: z.string().min(5, { message: t('regForm-zodFullname') }).max(40).refine(str => /^[^\d]+\s+[^\d]+(\s[^\d]+)*$/g.test(str), { message: t('regForm-zodInvalidName') })
+            .refine(str => !/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(str), { message: t('regForm-zodInvalidName') }),
         address: z.string().min(5, { message: t('regForm-zodAddress') }).refine(str => /[0-9]/.test(str), { message: t('regForm-zodAddress') }),
         idNumber: z.string().min(1),
         emailAddress: z.string().refine(str => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(str), { message: t('regForm-zodEmail') }),
