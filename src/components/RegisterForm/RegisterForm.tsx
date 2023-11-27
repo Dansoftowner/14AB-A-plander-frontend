4d981b2329bec6c9d7702f998eea4b3e893fe981
feherszabolcs <feherszabi2005@gmail.com>
2023-11-27 10:30:02 +0100

    :lipstick: Displayed error messages

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,10 +1,10 @@
-import { Button, Divider, HStack, Heading, Input, InputGroup, InputLeftAddon, InputRightElement, Stack, Text } from "@chakra-ui/react"
+import { Button, Divider, HStack, Heading, Input, InputGroup, InputLeftAddon, InputRightElement, Menu, MenuButton, MenuItem, MenuList, Stack, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from 'zod'
-// type RegisterForm = z.infer<typeof schema>
+
 
 export const RegisterForm = () => {
     const { i18n, t } = useTranslation()
@@ -17,16 +17,17 @@ export const RegisterForm = () => {
         username: z.string().min(5, { message: i18n.t('regForm-zodUsername') }).max(20),
         password: z.string().min(8, { message: t('regForm-zodPasswordLength') }).refine((str) => /[A-Z]/.test(str), { message: t('regForm-zodPassword') }).refine(str => /[0-9]/.test(str), { message: t('regForm-zodPassword') }),
         repeatedPassword: z.string(),
-        fullName: z.string().min(5, { message: t('regForm-zodFullname') }).max(40),
-        address: z.string().min(5, {message: t('regForm-zodAddress')}).refine(str => /[0-9]/.test(str), { message: t('regForm-zodAddress') }),
-        idNumber: z.number().int({message: t('regForm-zodIdNumber')}).gte(10000000000, {message: t('regForm-zodIdNumber')}).lte(9999999999, {message: t('regForm-zodIdNumber')}),
+        fullName: z.string().min(5, { message: t('regForm-zodFullname') }).max(40).refine(str => /^[a-zA-Z]+\s+[a-zA-z]/.test(str), { message: t('regForm-zodInvalidName') }),
+        address: z.string().min(5, { message: t('regForm-zodAddress') }).refine(str => /[0-9]/.test(str), { message: t('regForm-zodAddress') }),
+        idNumber: z.string().min(1),
         emailAddress: z.string().refine(str => /^[\w-\.]+@([\w-]+\.)[\w-]{2,4}$/g.test(str), { message: t('regForm-zodEmail') }),
-        phoneNumber: z.string()
-    }).refine(data => data.password === data.repeatedPassword, {
+        phoneNumber: z.string().min(1),
+    }).refine(data => data.password == data.repeatedPassword, {
         message: t('regForm-zodRepeatedPwd'),
         path: ["repeatedPassword"]
     })
 
+    //int({ message: t('regForm-zodIdNumber') }).gte(10000000000, { message: t('regForm-zodIdNumber') }).lte(9999999999, { message: t('regForm-zodIdNumber') }),
 
     type RegForm = z.infer<typeof schema>
 
@@ -43,17 +44,19 @@ export const RegisterForm = () => {
         if (i18n.language == 'en' && obj == 'personal') return 320
     }
 
+    if (errors) console.log(errors);
+
+
     return (
         <form className="mx-auto container mt-3" onSubmit={handleSubmit((e) => console.log(e))}>
             <Heading marginBottom={10}>{t('register-header')}</Heading>
             <HStack marginBottom={5}>
                 <Heading minWidth={setMinWidth('login')} size='lg'>{t('login-details')}</Heading>
-                <Divider borderColor={"grey"}/>
+                <Divider borderColor={"grey"} />
             </HStack>
             <Stack marginBottom={5}>
                 <div className="mb-3">
                     <Text>{t('username')}</Text>
-
                     <Input {...register('username')} width={400} placeholder={t('regForm-usernamePholder')} />
                     {errors.username && <p className='text-danger'>{errors.username.message?.toString()}</p>}
                 </div>
@@ -95,14 +98,17 @@ export const RegisterForm = () => {
                     <Text>{t('regForm-fullname')}</Text>
                     <Input {...register('fullName')} width={400} placeholder={t('regForm-fnPholder')} />
                 </div>
+                {errors.fullName && <p className="text-danger">{errors.fullName.message?.toString()}</p>}
                 <div className="mb-3">
                     <Text>{t('regForm-address')}</Text>
                     <Input {...register('address')} width={400} placeholder={t('regForm-adPholder')} />
                 </div>
+                {errors.address && <p className="text-danger">{errors.address.message?.toString()}</p>}
                 <div className="mb-3">
                     <Text>{t('regForm-idNumber')}</Text>
                     <Input {...register('idNumber')} width={400} placeholder={t('regForm-idcPholder')} />
                 </div>
+                {errors.idNumber && <p className="text-danger">{errors.idNumber.message?.toString()}</p>}
                 <div className="mb-3">
                     <Text>{t('regForm-email')}</Text>
                     <Input {...register('emailAddress')} width={400} placeholder={t('regForm-emaPholder')} />
@@ -115,6 +121,8 @@ export const RegisterForm = () => {
                         <Input {...register('phoneNumber')} width={340} type='tel' placeholder={t('regForm-phnPholder')} />
                     </InputGroup>
                 </div>
+                {errors.phoneNumber && <p className="text-danger">{errors.phoneNumber.message?.toString()}</p>}
+
             </Stack>
             <button className="btn btn-primary">{t('regForm-regButton')}</button>
         </form>
