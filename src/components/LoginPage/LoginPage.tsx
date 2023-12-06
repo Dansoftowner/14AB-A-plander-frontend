e8d1f0ff10227d3c6d13163c5bde89103a33142f
feherszabolcs <feherszabi2005@gmail.com>
2023-12-06 19:29:24 +0100

    :art: Login schema added

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -8,24 +8,25 @@ import {
 import { Autocomplete } from '@mui/material'
 import useAssociations, { Association } from '../../hooks/useAssociations'
 import { useMemo, useState } from 'react'
-import { Box, InputGroup, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, InputGroup, Stack, Text, useColorModeValue } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
-import { schema } from '../RegisterForm/inputSchema'
+import { loginSchema } from '../RegisterForm/inputSchema'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import PasswordInput from '../RegisterForm/PasswordInput'
+import FormInput from '../RegisterForm/FormInput'
 
 const LoginPage = () => {
   const cardBackground = useColorModeValue('#89D0DF', '#3393DF')
   const textColor = useColorModeValue('#0078D7', '#004881')
-  
+
   const { t } = useTranslation()
-    const inputSchema = useMemo(() => schema(t), [t])
+  const inputSchema = useMemo(() => loginSchema(t), [t])
 
-    type RegForm = z.infer<typeof inputSchema>
+  type LoginForm = z.infer<typeof inputSchema>
 
-    const { register, handleSubmit, formState: { errors } } = useForm<RegForm>({ resolver: zodResolver(inputSchema) })
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>({ resolver: zodResolver(inputSchema) })
 
   const associations = useAssociations({ limit: 10, projection: 'full' }).data
   const [selectedAssociation, setSelectedAssociation] =
@@ -40,32 +41,38 @@ const LoginPage = () => {
         color="white"
         h={600}
         w={500}
+        alignItems='center'
       >
         <Text
           color={textColor}
           fontSize="x-large"
           fontFamily="Moul"
           fontWeight="md"
-        >
-          Plander
-        </Text>
-
-        <PasswordInput register={register} name="password" errors={errors} required i18nPlaceHolder="regForm-pwdPholder" i18nTitle="password" />
+        > Plander</Text>
 
 
-        <ThemeProvider theme={createTheme()}>
-          <Autocomplete
-            options={associations}
-            getOptionLabel={(option) => option.name}
-            value={selectedAssociation || null}
-            renderInput={(params) => (
-              <TextField {...params} label="Válasszon egyesületet!" />
-            )}
-            onChange={(_event: any, value: Association | null) =>
-              setSelectedAssociation(value)
-            }
-          />
-        </ThemeProvider>
+
+
+
+        <Stack mt={10} alignItems='center'>
+          <ThemeProvider theme={createTheme()}>
+            <Autocomplete
+              options={associations}
+              fullWidth
+              getOptionLabel={(option) => option.name}
+              value={selectedAssociation || null}
+              renderInput={(params) => (
+                <TextField {...params} label="Válasszon egyesületet!" />
+              )}
+              onChange={(_event: any, value: Association | null) =>
+                setSelectedAssociation(value)
+              }
+            />
+          </ThemeProvider>
+
+          <FormInput login register={register} name="username" errors={errors} required={false} i18nPlaceHolder="Felhasználónév" />
+          <PasswordInput login register={register} name="password" errors={errors} required={false} i18nPlaceHolder="regForm-pwdPholder" i18nTitle="" />
+        </Stack>
 
         <Text>{selectedAssociation?.name}</Text>
         <Text>{selectedAssociation?.location}</Text>
