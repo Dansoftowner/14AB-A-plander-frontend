72e7aaa3756624d386986b1506da5fc725404af0
feherszabolcs <feherszabi2005@gmail.com>
2023-12-07 13:08:15 +0100

    :lipstick: Updated autocomplete

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -6,7 +6,7 @@ import {
 import { Autocomplete } from '@mui/material'
 import useAssociations, { Association } from '../../hooks/useAssociations'
 import { useMemo, useState } from 'react'
-import { Box, Stack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Checkbox, Icon, InputGroup, InputLeftAddon, InputLeftElement, InputRightElement, Menu, Stack, Text, useColorModeValue } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { loginSchema } from '../RegisterForm/inputSchema'
 import { z } from 'zod'
@@ -14,6 +14,16 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import PasswordInput from '../RegisterForm/PasswordInput'
 import FormInput from '../RegisterForm/FormInput'
+import {
+  AutoComplete,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+} from "@choc-ui/chakra-autocomplete";
+import { FaChevronDown } from "react-icons/fa";
+import { MdOutlineLocalPolice } from "react-icons/md";
+
+
 
 const LoginPage = () => {
   const cardBackground = useColorModeValue('#89D0DF', '#3393DF')
@@ -29,6 +39,8 @@ const LoginPage = () => {
   const associations = useAssociations({ limit: 10, projection: 'full' }).data
   const [selectedAssociation, setSelectedAssociation] =
     useState<Association | null>()
+  console.log(selectedAssociation);
+
 
   return (
     <>
@@ -52,31 +64,51 @@ const LoginPage = () => {
 
 
 
-        <Stack mt={10} alignItems='center'>
-          <ThemeProvider theme={createTheme()}>
-            <Autocomplete
-              options={associations}
-              fullWidth
-              getOptionLabel={(option) => option.name}
-              value={selectedAssociation || null}
-              renderInput={(params) => (
-                <TextField {...params} label="Válasszon egyesületet!" />
-              )}
-              onChange={(_event: any, value: Association | null) =>
-                setSelectedAssociation(value)
-              }
-            />
-          </ThemeProvider>
+        <Stack mt={10} alignItems='center' >
+          <Box width={400} margin={5}>
+            <AutoComplete openOnFocus onChange={(e: any, val: any) => setSelectedAssociation(val.originalValue)}>
+              <InputGroup>
+                <AutoCompleteInput placeholder="Válasszon egyesületet!"
+                  {...register("username")}
+                  borderRadius={10}
+                  fontSize={20}
+                  h={10} />
+                <InputRightElement
+                  children={<FaChevronDown />} />
+                <InputLeftElement>
+                  <MdOutlineLocalPolice />
+                </InputLeftElement>
+              </InputGroup>
+              <AutoCompleteList>
+                {associations.map((association, id) => (
+                  <AutoCompleteItem
+                    key={id}
+                    value={association}
+                    label={association.name}
+                    textTransform="capitalize"
+                  >
+                    {association.name}
+                  </AutoCompleteItem>
+                ))}
+              </AutoCompleteList>
+            </AutoComplete>
+          </Box>
 
-          <FormInput login register={register} name="username" errors={errors} required={false} i18nPlaceHolder="Felhasználónév" />
-          <PasswordInput login register={register} name="password" errors={errors} required={false} i18nPlaceHolder="regForm-pwdPholder" i18nTitle="" />
+          <Box margin={5}>
+            <FormInput login register={register} name="username" errors={errors} required={false} i18nPlaceHolder="Felhasználónév" />
+          </Box>
+          <Box margin={5}>
+            <PasswordInput login register={register} name="password" errors={errors} required={false} i18nPlaceHolder="regForm-pwdPholder" i18nTitle="" />
+          </Box>
+          <Checkbox>Maradjak bejelentkezve</Checkbox>
         </Stack>
+
 
         <Text>{selectedAssociation?.name}</Text>
         <Text>{selectedAssociation?.location}</Text>
         <Text>{selectedAssociation?._id}</Text>
         <Text>{selectedAssociation?.certificate}</Text>
-      </Box>
+      </Box >
     </>
   )
 }
