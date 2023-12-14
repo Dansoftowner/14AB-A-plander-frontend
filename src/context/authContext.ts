328bd573873f0b369a0d02e9c1a1358f6189be96
feherszabolcs <feherszabi2005@gmail.com>
2023-12-14 20:36:50 +0100

    :art: AuthContext with hook

--- src/context/authContext.ts
+++ src/context/authContext.ts
@@ -1,12 +1,9 @@
 import React, { SetStateAction } from 'react'
-import { AuthAction } from '../reducers/authReducer'
 import { User } from '../hooks/useMember'
 
 interface AuthContextType {
   user: User
-  setUser: React.Dispatch<AuthAction>
-  authToken: string
-  setAuthToken: React.Dispatch<SetStateAction<string>>
+  setUser: React.Dispatch<SetStateAction<User>>
 }
 export const AuthContext = React.createContext<AuthContextType>(
   {} as AuthContextType,
