a0580448fa0cc24204367ffefaf22fbf3e378d48
feherszabolcs <feherszabi2005@gmail.com>
2023-12-15 19:08:55 +0100

    :art: forgotten-password route created

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import LoginPage from './components/LoginPage/LoginPage'
 import { RegisterForm } from './components/RegisterForm/RegisterForm';
 import Layout from './components/Layout';
 import HomePage from './components/HomePage/HomePage';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 
 
 const router = createBrowserRouter([
@@ -17,11 +18,21 @@ const router = createBrowserRouter([
             {
                 path: '/register/:id/:registrationToken',
                 element: <RegisterForm />,
-                
+
             },
             {
                 path: '',
                 element: <HomePage />
+            },
+            {
+                path: 'forgotten-password',
+                element: <ForgotPassword />,
+                children: [
+                    {
+                        path: ':id/:restorationToken',
+                        element: <ForgotPassword />
+                    }
+                ]
             }
         ]
     }
