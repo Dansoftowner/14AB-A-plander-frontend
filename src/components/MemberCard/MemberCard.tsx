30289cb53f9723979505a730d2d4e9dec54185f0
feherszabi@sulid.hu <feherszabi@sulid.hu>
2023-12-21 14:27:42 +0100

    :iphone: Cards adjusted to mobiles

--- src/components/MemberCard/MemberCard.tsx
+++ src/components/MemberCard/MemberCard.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Stack, VStack, Text, Button } from '@chakra-ui/react'
+import { Box, HStack, Stack, VStack, Text, Button, Show } from '@chakra-ui/react'
 import { FaUserAlt, FaTrash } from "react-icons/fa";
 
 interface Props {
@@ -9,14 +9,16 @@ interface Props {
 
 const MemberCard = ({ email, name, phone }: Props) => {
     return (
-        <HStack direction='column' border='1px solid' borderRadius={4} padding={4} margin={2}>
-            <Stack width={55}>
-                <Text fontSize={40}><FaUserAlt /></Text>
-            </Stack>
+        <HStack direction='column' maxW='95vw' border='1px solid' borderRadius={4} padding={4} margin={2}>
+            <Show above='lg'>
+                <Stack width={55}>
+                    <Text fontSize={40}><FaUserAlt /></Text>
+                </Stack>
+            </Show>
             <VStack textAlign='start'>
-                <Text width={400} margin={1}><b>Név:</b> {name}</Text>
-                <Text width={400} margin={1}><b>Email cím:</b> {email}</Text>
-                <Text width={400} margin={1}><b>Telefonszám:</b> {phone}</Text>
+                <Text width={400} maxW='70vw' margin={1}><b>Név:</b> {name}</Text>
+                <Text width={400} maxW='70vw' margin={1}><b>Email cím:</b> {email}</Text>
+                <Text width={400} maxW='70vw' margin={1}><b>Telefonszám:</b> {phone}</Text>
             </VStack>
             <Box ml='auto'>
                 <Button >
