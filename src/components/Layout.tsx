b1bccca16d631b1d23d7956c76806e5186bc630f
feherszabi@sulid.hu <feherszabi@sulid.hu>
2023-12-21 12:16:25 +0100

    :lipstick: Navigation update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import { AuthContext } from '../context/authContext'
 import NavBar from './NavBar/NavBar'
-import { Link, Navigate, Outlet, useLocation } from 'react-router-dom'
-import { Button, Grid, GridItem, HStack, Show, useColorModeValue } from '@chakra-ui/react'
+import { Link, NavLink, Navigate, Outlet, useLocation } from 'react-router-dom'
+import { Button, Grid, GridItem, Text, Show, useColorModeValue } from '@chakra-ui/react'
 import MembersList from './MembersList/MembersList'
 
 const Layout = () => {
@@ -10,6 +10,7 @@ const Layout = () => {
     const buttonBg = useColorModeValue('#0078d7', '#fde74c')
     const buttonColor = useColorModeValue('#ffffff', '#004881')
     const buttonHover = useColorModeValue('#0078b0', '#fde7af')
+    const textColor = useColorModeValue('#000000', '#ffffff')
 
     const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user')!) || JSON.parse(localStorage.getItem('user')!))
 
@@ -42,11 +43,18 @@ const Layout = () => {
                     </GridItem>
 
 
-                    <GridItem area='nav' backgroundColor='red'>
+                    <GridItem area='nav'>
                         <Show above='lg'>
-                            <Button backgroundColor={buttonBg} color={buttonColor} type='submit' _hover={{ backgroundColor: buttonHover }}>
-                                <Link to='/members'>Membörsz</Link>
-                            </Button>
+                            <NavLink to='/' style={({ isActive }) => ({
+                                color: isActive ? buttonBg : textColor,
+                            })}>
+                                <Text fontSize={30} padding={3} height={20} _hover={{ fontSize: 32, transition: ' 0.1s ease-in-out' }}>Főoldal</Text>
+                            </NavLink>
+                            <NavLink to='/members' style={({ isActive }) => ({
+                                color: isActive ? buttonBg : textColor,
+                            })}>
+                                <Text fontSize={30} padding={3} height={20} _hover={{ fontSize: 32, transition: ' 0.1s ease-in-out' }}>Tagok</Text>
+                            </NavLink>
                         </Show>
                     </GridItem>
 
