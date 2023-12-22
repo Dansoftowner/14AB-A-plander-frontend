984a6bd80a6bfa84ccc933356061ba41a481fd04
feherszabolcs <feherszabi2005@gmail.com>
2023-12-22 16:43:01 +0100

    :sparkles: Invitation implemented

---
+++ src/components/MembersList/InviteMember.tsx
@@ -0,0 +1,81 @@
+import { useMemo } from 'react'
+import useAuth from '../../hooks/useAuth'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { Box, Button, HStack, Stack, useColorModeValue, useToast } from '@chakra-ui/react'
+import FormInput from '../RegisterForm/FormInput'
+import { inviteSchema } from '../RegisterForm/inputSchema'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import apiClient from '../../services/apiClient'
+
+const InviteMember = () => {
+    const buttonBg = useColorModeValue('#0078d7', '#fde74c')
+    const buttonColor = useColorModeValue('#ffffff', '#004881')
+    const buttonHover = useColorModeValue('#0078b0', '#fde7af')
+
+    const { user } = useAuth()
+    const navigate = useNavigate()
+    const inviteToast = useToast()
+
+    const { t } = useTranslation('register')
+    const inputSchema = useMemo(() => inviteSchema(t), [t])
+    type InviteForm = z.infer<typeof inputSchema>
+    const { register, handleSubmit, formState: { errors } } = useForm<InviteForm>({ resolver: zodResolver(inputSchema) })
+
+
+    if (!user) return <Navigate to='/login' />
+    if (!user.roles.includes('president')) return <Navigate to='/members' />
+    return (
+        <Box mt={20} maxW='95vw' mx={5}>
+            <form onSubmit={handleSubmit((e) => {
+                const invited = Object.fromEntries(Object.entries(e).filter(([key, value]) => value != ""))
+                apiClient.post('/members', invited, {
+                    headers: {
+                        'x-plander-auth':
+                            localStorage.getItem('token') || sessionStorage.getItem('token'),
+                    }
+                }).then(res => {
+                    inviteToast({
+                        title: res.status === 202 ? t('success') : t('error'),
+                        status: res.status === 202 ? 'success' : 'error',
+                        duration: 9000,
+                        isClosable: true,
+                        position: 'top'
+                    })
+                })
+                    .catch(err => {
+                        inviteToast({
+                            title: t('error'),
+                            status: 'error',
+                            description: err.response.data.message,
+                            duration: 9000,
+                            isClosable: true,
+                            position: 'top'
+                        })
+                    })
+            })}>
+                <Stack marginBottom={5} align='center' mx={5}>
+                    <FormInput register={register} name="email" i18nPlaceHolder="emailPHolder" i18nTitle="email" required={true} errors={errors} />
+                    <FormInput register={register} name="name" i18nPlaceHolder="fnPholder" i18nTitle="fullname" required={false} errors={errors} />
+                    <FormInput register={register} name="address" i18nPlaceHolder="adPholder" i18nTitle="address" required={false} errors={errors} />
+
+                    <FormInput register={register} name="idNumber" i18nPlaceHolder="idcPholder" i18nTitle="idNumber" required={false} errors={errors} />
+
+                    <FormInput register={register} guard name="guardNumber" i18nPlaceHolder="guardNumPholder" i18nTitle="guardNumber" required={false} errors={errors} />
+
+                    <FormInput register={register} tel name="phoneNumber" i18nPlaceHolder="phnPholder" i18nTitle="phone" required={false} errors={errors} />
+
+                </Stack>
+
+                <HStack justifyContent='center' mx={5}>
+                    <Button mr='auto' _hover={{ backgroundColor: buttonHover }} backgroundColor={buttonBg} color={buttonColor} onClick={() => navigate('/members')}>Vissza</Button>
+                    <Button mr='auto' name="submitbtn" type="submit" _hover={{ backgroundColor: buttonHover }} color={buttonColor} backgroundColor={buttonBg}>{t('regButton')}</Button>
+                </HStack>
+            </form>
+        </Box >
+    )
+}
+
+export default InviteMember
