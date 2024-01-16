caad3cebe6de5c3ac575452226971df59020441e
feherszabolcs <feherszabi2005@gmail.com>
2024-01-16 14:10:01 +0100

    :art: Role-transfer moved into hook

--- src/hooks/useTransfer.ts
+++ src/hooks/useTransfer.ts
@@ -1,10 +1,18 @@
 import apiClient from '../services/apiClient'
+import i18n from '../i18n'
 
-const useTransfer = (_id: string, password: string) =>
-  apiClient.patch(`/members/transfer-my-roles/${_id}?q=true`, {
-    headers: {
-      'x-plander-auth':
-        localStorage.getItem('token') || sessionStorage.getItem('token'),
-      'x-current-pwd': password,
-    },
-  })
+export const useTransfer = (_id: string, password: string) =>
+  apiClient
+    .patch(`/members/transfer-my-roles/${_id}`, '', {
+      params: {
+        copy: true,
+      },
+      headers: {
+        'x-plander-auth':
+          localStorage.getItem('token') || sessionStorage.getItem('token'),
+        'x-current-pwd': password,
+        'Accept-Language': i18n.language,
+      },
+    })
+    .then((res) => res)
+    .catch((err) => err)
