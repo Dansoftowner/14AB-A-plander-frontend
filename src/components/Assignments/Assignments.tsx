b0a3d22a7bb373f80ca6244ff858d6de9dac959a
feherszabolcs <feherszabi2005@gmail.com>
2024-01-26 11:36:20 +0100

    :construction: Add assignment

--- src/components/Assignments/Assignments.tsx
+++ src/components/Assignments/Assignments.tsx
@@ -1,11 +1,58 @@
 import React from 'react'
 import CalendarComponent from './CalendarComponent'
+import { AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, Button, useDisclosure, useColorModeValue, Stack } from '@chakra-ui/react'
+import { useAuth } from '../../hooks/hooks'
 
 const Assignments = () => {
+
+    const { user } = useAuth()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = React.useRef<HTMLButtonElement>(null)
+
+    const buttonBg = useColorModeValue('#0078d7', '#fde74c')
+    const buttonColor = useColorModeValue('#ffffff', '#004881')
+    const buttonHover = useColorModeValue('#0078b0', '#fde7af')
+
+
     return (
         <div>
             <CalendarComponent />
-        </div>
+            {user.roles?.includes('president') &&
+                <Stack alignItems='center' mt={10}>
+                    <Button width={250} backgroundColor={buttonBg} color={buttonColor} _hover={{ backgroundColor: buttonHover }} onClick={onOpen}>Beosztás felvétele</Button>
+                </Stack>
+            }
+
+
+
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+                closeOnEsc={true}
+            >
+                <AlertDialogOverlay >
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            Beosztás rögzítése
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure? You can't undo this action afterwards.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Mégse
+                            </Button>
+                            <Button colorScheme='green' onClick={onClose} ml={3}>
+                                Rögzítés
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </div >
     )
 }
 
