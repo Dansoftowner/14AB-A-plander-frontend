1b9811b515e03b9700b55ba937228401678d34d6
feherszabolcs <feherszabi2005@gmail.com>
2024-01-29 14:01:07 +0100

    :construction: Design edited

--- src/components/Assignments/AddAssignment.tsx
+++ src/components/Assignments/AddAssignment.tsx
@@ -1,7 +1,7 @@
-import { Button, HStack, Heading, InputGroup, InputLeftElement, InputRightElement, List, ListItem, Spinner, Text, UnorderedList, border } from "@chakra-ui/react"
+import { Box, Button, Divider, FormControl, FormLabel, HStack, Heading, Input, InputGroup, InputLeftElement, InputRightElement, List, ListItem, Spinner, Text, UnorderedList, VStack, border, useColorModeValue } from "@chakra-ui/react"
 import { AutoComplete, AutoCompleteInput, AutoCompleteList, AutoCompleteItem } from "@choc-ui/chakra-autocomplete"
 import { t } from "i18next"
-import { Fragment, useState } from "react"
+import { Dispatch, Fragment, SetStateAction, useState } from "react"
 import { FaChevronDown } from "react-icons/fa"
 import { MdOutlineLocalPolice } from "react-icons/md"
 import { useInfiniteMembers } from "../../hooks/useMembers"
@@ -11,30 +11,53 @@ import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
 import '@wojtekmaj/react-datetimerange-picker/dist/DateTimeRangePicker.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
+import './calendar.css'
+import i18n from "../../i18n"
 
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const AddAssignment = () => {
+interface Props {
+    inDuty: User[],
+    setInDuty: Dispatch<SetStateAction<User[]>>
+    value: Value,
+    setValue: Dispatch<SetStateAction<Value>>
+    
+}
+
+const AddAssignment = ({ inDuty, setInDuty, value, setValue }: Props) => {
 
     const [qParam, setQParam] = useState('')
     const { data: members, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } = useInfiniteMembers({ limit: 4, projection: 'lite', q: qParam })
     const [selectedMember, setSelectedMember] = useState({} as User)
 
-    const [inDuty, setInDuty] = useState([] as User[])
-
-    const [value, onChange] = useState<Value>(new Date());
 
     return (
         <>
+            <VStack>
+                <FormControl my={5}>
+                    <FormLabel>Szolgálat neve</FormLabel>
+                    <InputGroup>
+                        <Input placeholder='Nem kötelező.' />
+                    </InputGroup>
+                </FormControl>
+
+                <FormControl mb={10}>
+                    <FormLabel>Szolgálat helyszíne</FormLabel>
+                    <InputGroup>
+                        <Input placeholder='Nem kötelező.' />
+                    </InputGroup>
+                </FormControl>
+            </VStack>
             <HStack>
+
                 <AutoComplete freeSolo openOnFocus onChange={(_e: any, val: any) => setSelectedMember(val.originalValue)} isLoading={isLoading} emptyState={<Text textAlign='center'>Nincs ilyen egyesület!</Text>}>
                     <InputGroup>
                         <AutoCompleteInput autoComplete="off" placeholder='Tag neve' value={selectedMember.name || ''}
                             borderRadius={10}
-                            fontSize={20}
+                            fontSize={18}
                             h={10}
                             onChange={(val: any) => {
                                 setQParam(val.target.value)
@@ -67,7 +90,11 @@ const AddAssignment = () => {
                     </AutoCompleteList>
                 </AutoComplete>
                 <Button onClick={() => {
-                    setInDuty([...inDuty, selectedMember])
+                    if (selectedMember._id) {
+                        setInDuty([...inDuty, selectedMember])
+                    }
+                    console.log(value)
+                    console.log(inDuty)
                     setSelectedMember({} as User)
                 }}><Text mb={0}>Felvétel</Text></Button>
             </HStack >
@@ -81,9 +108,12 @@ const AddAssignment = () => {
                 ))}
             </List>
 
+            <Divider />
+
             <Heading mt={5} fontSize='medium'>Szolgálat ideje</Heading>
-            <DateTimeRangePicker value={value} onChange={onChange} />
-                    {/*Minden szolgálat 3 órás? --> akk nem kell range picker, csak picked+3*/ }
+            <Box>
+                <DateTimeRangePicker locale={i18n.language == 'hu' ? 'hu-HU' : 'en-US'} value={value} onChange={setValue} />
+            </Box>
         </>
     )
 }
--- src/components/Assignments/Assignments.tsx
+++ src/components/Assignments/Assignments.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CalendarComponent from './CalendarComponent'
 import { AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, Button, useDisclosure, useColorModeValue, Stack } from '@chakra-ui/react'
 import { useAuth } from '../../hooks/hooks'
 import AddAssignment from './AddAssignment'
+import { User } from '../../hooks/useLogin'
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const Assignments = () => {
 
@@ -13,6 +18,11 @@ const Assignments = () => {
     const buttonBg = useColorModeValue('#0078d7', '#fde74c')
     const buttonColor = useColorModeValue('#ffffff', '#004881')
     const buttonHover = useColorModeValue('#0078b0', '#fde7af')
+
+    //for prop passing
+    const [inDuty, setInDuty] = useState([] as User[])
+
+    const [value, setValue] = useState<Value>(new Date());
 
 
     return (
@@ -39,11 +49,15 @@ const Assignments = () => {
                         </AlertDialogHeader>
 
                         <AlertDialogBody>
-                            <AddAssignment />
+                            <AddAssignment inDuty={inDuty} setInDuty={setInDuty} value={value} setValue={setValue} />
                         </AlertDialogBody>
 
                         <AlertDialogFooter>
-                            <Button ref={cancelRef} onClick={onClose}>
+                            <Button ref={cancelRef} onClick={() => {
+                                onClose()
+                                setInDuty([])
+                                setValue(new Date())
+                            }}>
                                 Mégse
                             </Button>
                             <Button colorScheme='green' onClick={onClose} ml={3}>
