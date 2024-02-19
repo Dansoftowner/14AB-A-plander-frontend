93bc7b8aeb44f58a762b534d95ff0259934450a2
feherszabolcs <feherszabi2005@gmail.com>
2024-02-19 17:42:33 +0100

    :sparkles: Theme added to preferences

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,14 +1,27 @@
 import { HStack, useColorMode, Button } from '@chakra-ui/react'
 import { BsFillSunFill } from "react-icons/bs";
 import { BsMoonStarsFill } from "react-icons/bs";
+import useAuth from '../hooks/useAuth';
+import { useEffect } from 'react';
 
 
 
 const ColorModeSwitch = () => {
-    const { toggleColorMode, colorMode } = useColorMode()
+    const { colorMode, setColorMode } = useColorMode()
+    const { preferences, setPreferences } = useAuth()
+
+    const setColor = () => {
+        setColorMode(colorMode === 'light' ? 'dark' : 'light')
+        setPreferences({ ...preferences, colorMode: colorMode === 'light' ? 'dark' : 'light' })
+    }
+
+    useEffect(() => {
+        setColorMode(preferences?.colorMode || colorMode)
+    }, [preferences])
+
     return (
         <HStack>
-            <Button onClick={toggleColorMode} backgroundColor='transparent'>{colorMode === 'light' ? <BsFillSunFill/> : <BsMoonStarsFill/>}</Button>
+            <Button onClick={setColor} backgroundColor='transparent'>{colorMode === 'light' ? <BsFillSunFill /> : <BsMoonStarsFill />}</Button>
         </HStack>
     )
 }
