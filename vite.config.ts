d641ea4df8a8a36ae7fa79ed3e13238ec11dfcaf
feherszabolcs <feherszabi2005@gmail.com>
2024-02-27 16:48:47 +0100

    :bug: Invalid env

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    __APP_ENV__: process.env.VITE_BACKEND_URL
+  }
 })
